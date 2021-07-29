d523002683a60584e9b27669882df8055ef28dfb
Anya Tamara <anyatmr@gmail.com>
2021-07-29 11:04:05 +0700

    add view

---
+++ Esmeka-MC3/Esmeka-MC3/Features/BriefView/BriefViewController.swift
@@ -0,0 +1,54 @@
+//
+//  BriefViewController.swift
+//  Esmeka-MC3
+//
+//  Created by Anya Akbar on 27/07/21.
+//
+
+import UIKit
+
+class BriefViewController: UIViewController {
+
+    private var isChecked = false
+    @IBOutlet weak var briefTable: UITableView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var ShowAgainSwitch: UISwitch!
+    @IBOutlet weak var showAgainButton: UIButton!
+ 
+    var briefInstructions : [BriefModel] = BriefModel.BriefData
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setView()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Setup View
+    func setView() {
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 34)
+        briefTable.register(BriefCell.nib, forCellReuseIdentifier: BriefCell.identifier)
+        briefTable.dataSource = self
+        briefTable.delegate = self
+        briefTable.layer.cornerRadius = 10
+        startButton.layer.cornerRadius = 25
+  
+    }
+
+}
+
+extension BriefViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        briefInstructions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = briefTable.dequeueReusableCell(withIdentifier: BriefCell.identifier) as! BriefCell
+        
+        cell.titleLabel.text = briefInstructions[indexPath.row].title
+        cell.contentLabel.text = briefInstructions[indexPath.row].content
+        return cell
+    }
+    
+    
+}
