c2a017c8ca0953d6b748a236617f1b779bb8f27c
Christopher Teddy Mienarto <46671469+christoted@users.noreply.github.com>
2021-08-02 15:03:01 +0700

    feat/video-player-done

--- Esmeka-MC3/Esmeka-MC3/Features/Detail/View/Custom View/VideoTableViewCell.swift
+++ Esmeka-MC3/Esmeka-MC3/Features/Detail/View/Custom View/VideoTableViewCell.swift
@@ -6,19 +6,35 @@
 //
 
 import UIKit
+protocol VideoProtocolDelegate: AnyObject {
+    func playVideo()
+}
 
 class VideoTableViewCell: UITableViewCell {
+    
+    static let instance = VideoTableViewCell()
 
     @IBOutlet weak var videoContainerView: UIView!
     @IBOutlet weak var ivPlay: UIImageView!
     @IBOutlet weak var ivThumbail: UIImageView!
     @IBOutlet weak var lblVideo: UILabel!
+    
+    weak var videoDelegate: VideoProtocolDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         setupView()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(onTap))
+        ivPlay.isUserInteractionEnabled = true
+        ivPlay.addGestureRecognizer(tap)
     }
-
+    
+    @objc func onTap(){
+        videoDelegate?.playVideo()
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
