118d902a36a73dfd320135577d8c0295ef04fe60
Adhella <adhellasbl@gmail.com>
2021-08-07 04:37:06 +0700

    Video saving and duration
    - video saved to gallery, you can fetch the video's url by saving its PHAsset (need revision on database)
    - the duration can be fetched through PHAsset.duration (look at saveData() on InterviewSimulationController and it returns the duration in seconds)

---
+++ Esmeka-MC3/Esmeka-MC3/Features/InterviewSimulation/MainPage/VideoFetchClass.swift
@@ -0,0 +1,67 @@
+//
+//  VideoFetchClass.swift
+//  Esmeka-MC3
+//
+//  Created by Adhella Subalie on 07/08/21.
+//
+
+import UIKit
+import Photos
+class VideoFetchClass{
+    func loadLastVideo()->PHAsset{
+        
+        let fetchResult =  PHAsset.fetchAssets(with: .video, options: fetchOptions()).lastObject
+        PHImageManager().requestAVAsset(forVideo: fetchResult!, options: nil, resultHandler: { (avurlAsset, audioMix, dict) in
+            if let newObj = avurlAsset as? AVURLAsset{
+                print(newObj.url)
+            } else {
+            }
+        })
+        return fetchResult!
+    }
+    
+    func areDifferentAssets(asset1:PHAsset, asset2:PHAsset)->Bool{
+        var isDifferent = false
+        asset1.getURL { url1 in
+            asset2.getURL { url2 in
+                if url1==url2{
+                    isDifferent = true
+                }
+            }
+        }
+        return isDifferent
+    }
+    
+    func fetchOptions() -> PHFetchOptions {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        return fetchOptions
+    }
+    
+}
+
+extension PHAsset {
+
+    func getURL(completionHandler : @escaping ((_ responseURL : URL?) -> Void)){
+        if self.mediaType == .image {
+            let options: PHContentEditingInputRequestOptions = PHContentEditingInputRequestOptions()
+            options.canHandleAdjustmentData = {(adjustmeta: PHAdjustmentData) -> Bool in
+                return true
+            }
+            self.requestContentEditingInput(with: options, completionHandler: {(contentEditingInput: PHContentEditingInput?, info: [AnyHashable : Any]) -> Void in
+                completionHandler(contentEditingInput!.fullSizeImageURL as URL?)
+            })
+        } else if self.mediaType == .video {
+            let options: PHVideoRequestOptions = PHVideoRequestOptions()
+            options.version = .original
+            PHImageManager.default().requestAVAsset(forVideo: self, options: options, resultHandler: {(asset: AVAsset?, audioMix: AVAudioMix?, info: [AnyHashable : Any]?) -> Void in
+                if let urlAsset = asset as? AVURLAsset {
+                    let localVideoUrl: URL = urlAsset.url as URL
+                    completionHandler(localVideoUrl)
+                } else {
+                    completionHandler(nil)
+                }
+            })
+        }
+    }
+}
