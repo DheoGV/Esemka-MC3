9110163c9bb16e9c07cf7a0556b896ec827f0d6f
Dheo Gildas Varian <dheogildasvarian@gmail.com>
2021-08-20 10:10:39 +0700

    filter masih error

---
+++ Esmeka-MC3/Esmeka-MC3/Features/DaftarSimulasi/View/Header Cell/SectionCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  SectionCollectionReusableView.swift
+//  Esmeka-MC3
+//
+//  Created by Dheo Gildas Varian on 20/08/21.
+//
+
+import UIKit
+
+class SectionCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
