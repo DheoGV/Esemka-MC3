753173c66d9d87e0dd8287537c05ad925775c5e0
ted <vitedocorp@yahoo.com>
2021-08-25 09:15:06 +0700

    feat/watch

--- Esmeka-MC3/Esemka-WatchKit WatchKit Extension/ExtensionDelegate.swift
+++ Esmeka-MC3/Esemka-WatchKit WatchKit Extension/ExtensionDelegate.swift
@@ -8,7 +8,11 @@
 import WatchKit
 import WatchConnectivity
 
-class ExtensionDelegate: NSObject, WKExtensionDelegate {
+class ExtensionDelegate: NSObject, WKExtensionDelegate, WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
 
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
